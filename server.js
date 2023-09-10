e9cba411798eb24248501ddd3ea3b6ebcabc76f2
lukasalencar <lucas.kauan@aluno.ifsp.edu.br>
2023-09-10 15:04:42 -0300

    API node com postgres

---
+++ server.js
@@ -0,0 +1,65 @@
+/* import { createServer } from 'node:http'
+
+const server = createServer((request, response) =>{
+    response.write('Hello World')
+
+    return response.end();
+})
+
+server.listen(3333) */
+
+import { fastify } from "fastify";
+import { DatabasePostgres } from "./database-postgres.js";
+/* import { DatabaseMemory } from "./database-memory.js" */
+
+const server = fastify()
+/* const db = new DatabaseMemory() */
+const db = new DatabasePostgres()
+
+
+server.get('/videos', async (request)=>{
+    const search = request.query.search
+
+    const videos = await db.list(search)
+
+    return videos
+})
+
+// Request Body
+
+server.post('/videos', async (request, reply)=>{
+    const { title, description, duration} = request.body    
+    
+    await db.create({
+        title,
+        description,
+        duration,
+    })
+ 
+    return reply.status(201).send()
+})
+
+server.put('/videos/:id', async (request, reply)=>{
+    const videoId = request.params.id
+    const { title, description, duration} = request.body 
+    
+    await db.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', async (request, reply)=>{
+    const videoId = request.params.id
+
+    await db.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+server.listen({
+    port: process.env.PORT ?? 3333,
+})
